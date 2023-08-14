64209498137ede2ca14d4b342dd4b44bcda624e2
kknarru <kknarru@gmail.com>
2023-08-14 10:56:06 -0400

    tVSignal.Period.Contains("1")

--- DotNetCoreSqlDb/Controllers/TVSignalController.cs
+++ DotNetCoreSqlDb/Controllers/TVSignalController.cs
@@ -99,7 +99,7 @@ namespace DotNetCoreSqlDb.Controllers
 
             if (tVSignal.Simbol == "SPX")
             {
-                if (tVSignal.Period.Contains("3") || tVSignal.Period.Contains("3M") )
+                if (tVSignal.Period.Contains("1"))
                 {
                     var hoursLeft = 16 - DateTime.Now.Hour;
                     double awayFactor = (hoursLeft / 3) * 10;
