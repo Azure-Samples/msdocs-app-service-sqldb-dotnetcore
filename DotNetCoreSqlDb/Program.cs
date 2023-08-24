c1087229baf6fddeb392b6b396d22e92eea16769
James Patten <james.patten@fidelity-energy.co.uk>
2023-08-24 10:00:50 +0100

    Moved DotNetCoreSqlDb Into Github workflows

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -28,7 +28,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     // app.UseHsts();
 }
-
+ 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
