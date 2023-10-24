efe56c0cad3a7541c996e5a23a313718cb356391
robinkotsch <75489778+robinkotsch@users.noreply.github.com>
2023-10-24 11:41:16 +0200

    connection string in Program.cs

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -3,14 +3,28 @@ using Microsoft.Extensions.DependencyInjection;
 using DotNetCoreSqlDb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+
+string connectionString = "Server=todoiiot-server.postgres.database.azure.com;Database=todoiiot-database;Port=5432;Ssl Mode=Require;User Id=dcaigpqaja;Password=7337L4X67TVUO6F3$;";
+
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING")));
+
+    options.UseSqlServer(connectionString));
+
+
 builder.Services.AddStackExchangeRedisCache(options =>
 {
 options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
 options.InstanceName = "SampleInstance";
 });
+
+
+
+
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
