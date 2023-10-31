c0e053919b264f4e5d2ef9780969b5c62c16a6f6
robinkotsch <robin.kotsch@gmail.com>
2023-10-31 10:22:40 +0100

    postgreSgl connection

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -5,17 +5,34 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using DotNetCoreSqlDb.Data;
 using System.Diagnostics;
+using Npgsql;
 using System.Runtime.Intrinsics.X86;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 //builder.Services.AddSqlServer<MyDatabaseContext>(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING"));
 
-
+/*
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING")));
+*/
+
+
+// Best practice is to scope the NpgsqlConnection to a "using" block
+using (NpgsqlConnection conn = new NpgsqlConnection(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING")))
+{
+    // Connect to the database
+    conn.Open();
+
+
+}
+
+
+
+
 
 
 builder.Services.AddStackExchangeRedisCache(options =>
