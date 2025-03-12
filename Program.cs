421660db37cb1ece04ebf2cbe92717a55705d65a
mihailchlv <mihail.cholakov98@gmail.com>
2025-03-12 14:42:53 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,11 +13,7 @@ else
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-    builder.Services.AddStackExchangeRedisCache(options =>
-    {
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options.InstanceName = "SampleInstance";
-    });
+    builder.Services.AddDistributedMemoryCache();
 }
 
 
