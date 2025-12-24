9fabf87118658dd996b8ebcdc1c924e67343afc9
roerstrand <129670382+roerstrand@users.noreply.github.com>
2025-12-24 16:33:29 +0000

    e

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using DotNetCoreSqlDb.Data;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add database context and cache
-if(builder.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+
     builder.Services.AddDistributedMemoryCache();
 }
 else
 {
-    // SQL – use AZURE_SQL_CONNECTIONSTRING
+    // ---------- SQL (MÅSTE FINNAS) ----------
     var sqlConnectionString =
         builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")
         ?? builder.Configuration["AZURE_SQL_CONNECTIONSTRING"];
@@ -19,36 +21,41 @@ else
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(sqlConnectionString));
 
-    // Redis – use AZURE_REDIS_CONNECTIONSTRING
-   var redisConnectionString =
-    builder.Configuration.GetConnectionString("AZURE_REDIS_CONNECTIONSTRING")
-    ?? builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+    // ---------- REDIS (VALFRI / SÄKER) ----------
+    var redisConnectionString =
+        builder.Configuration.GetConnectionString("AZURE_REDIS_CONNECTIONSTRING")
+        ?? builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
 
     if (!string.IsNullOrWhiteSpace(redisConnectionString))
-{
-
-
-    builder.Services.AddStackExchangeRedisCache(options =>
     {
-        options.Configuration = redisConnectionString;
-        options.InstanceName = "SampleInstance";
-    });
-}
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.ConfigurationOptions = new ConfigurationOptions
+            {
+                EndPoints = { redisConnectionString },
+                AbortOnConnectFail = false,
+                ConnectTimeout = 5000,
+                SyncTimeout = 5000
+            };
+
+            options.InstanceName = "SampleInstance";
+        });
+    }
+    else
+    {
+        builder.Services.AddDistributedMemoryCache();
+    }
 }
 
-// Add services to the container.
+// MVC
 builder.Services.AddControllersWithViews();
-
-// Add App Service logging
 builder.Logging.AddAzureWebAppDiagnostics();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -60,9 +67,7 @@ using (var scope = app.Services.CreateScope())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
